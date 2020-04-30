500c0f688a99e8c9b4219c2108ff8442ffc6b6ab
anita1218 <62133677+anita1218@users.noreply.github.com>
2020-04-30 20:08:51 +0800

    Update settings.py

--- students/settings.py
+++ students/settings.py
@@ -33,7 +33,7 @@ from linebot.models import *
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'U'
+SECRET_KEY = 'Ub8e3cf75739079f25a50f82b2cbd4c63'
 # 必須放上自己的Channel Access Token
 LINE_CHANNEL_ACCESS_TOKEN ='I4SBX13m+Q4RYMQI2wKw3U3OaZVkVc9YJN+wi7lLD4lqTIub5GulT/BNolulMzl32cjDfrjByvy47lGvTtn7hjNgSetN56HjWmG5ajCXtYICjL+25OCUO5v/RQIicSpR6RQQM6u24wxNoX3nMpBZPwdB04t89/1O/w1cDnyilFU='
 # 必須放上自己的Channel Secret
