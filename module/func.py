d34e0c6202dd971c69606a5a5e304bce22b30e7e
anita1218 <62133677+anita1218@users.noreply.github.com>
2020-05-08 16:36:48 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -465,4 +465,3 @@ def manageForm(event, mtext):
         line_bot_api.reply_message(event.reply_token,message)
     except:
         line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤！'))
-
