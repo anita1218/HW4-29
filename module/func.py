9c23f32e409e8513a6ba9050f23c51c62a97e250
anita1218 <62133677+anita1218@users.noreply.github.com>
2020-05-08 15:47:34 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -98,20 +98,20 @@ def sendButton(event):  #按鈕樣版
             alt_text='按鈕樣板',
             template=ButtonsTemplate(
                 thumbnail_image_url='https://i.imgur.com/SKw73MQ.png',  #顯示的圖片
-                title='按鈕樣版示範',  #主標題
-                text='請選擇：',  #副標題
+                title='alice',  #主標題
+                text='個人頁面：',  #副標題
                 actions=[
                     MessageTemplateAction(  #顯示文字計息
                         label='文字訊息',
-                        text='@牛排'
+                        text='@自己'
                     ),
                     URITemplateAction(  #開啟網頁
                         label='連結銘傳網站',
-                        uri='http://www.e-happy.com.tw'
+                        uri='https://web.mcu.edu.tw/'
                     ),
                     PostbackTemplateAction(  #執行Postback功能,觸發Postback事件
                         label='回傳訊息',  #按鈕文字
-                        #text='@牛排',  #顯示文字計息
+                        #text='@資料',  #顯示文字計息
                         data='action=buy'  #Postback資料
                     ),
                 ]
