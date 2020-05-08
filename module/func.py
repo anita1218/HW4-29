fc31636dc4b8b75dce29a6df8426cb4434f65599
anita1218 <62133677+anita1218@users.noreply.github.com>
2020-05-08 15:08:02 +0800

    Update func.py

--- module/func.py
+++ module/func.py
@@ -97,7 +97,7 @@ def sendButton(event):  #按鈕樣版
         message = TemplateSendMessage(
             alt_text='按鈕樣板',
             template=ButtonsTemplate(
-                thumbnail_image_url='https://i.imgur.com/4QfKuz1.png',  #顯示的圖片
+                thumbnail_image_url='https://i.imgur.com/SKw73MQ.png',  #顯示的圖片
                 title='按鈕樣版示範',  #主標題
                 text='請選擇：',  #副標題
                 actions=[
